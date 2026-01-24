84c96951a4d182d3c2c0eb0d4dd5b113b4b2f9ea
Andreas Knopke <andreasknopke@gmail.com>
2026-01-24 13:16:53 +0000

    Trigger frontend redeploy - custom categories feature

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// v1.0.3 - Custom categories in scheduler
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <App />
