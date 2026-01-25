f7e25e8cdc98e620e3b2d17f11a501e1b804c606
Andreas Knopke <andreasknopke@gmail.com>
2026-01-25 10:01:35 +0000

    fix: Convert is_specialist to boolean in dbProxy

--- server/routes/dbProxy.js
+++ server/routes/dbProxy.js
@@ -50,7 +50,7 @@ const fromSqlRow = (row) => {
       'receive_email_notifications', 'exclude_from_staffing_plan', 
       'user_viewed', 'auto_off', 'show_in_service_plan', 
       'allows_rotation_concurrently', 'allows_consecutive_days', 
-      'acknowledged', 'is_active'
+      'acknowledged', 'is_active', 'is_specialist'
     ];
     if (boolFields.includes(key)) {
       res[key] = !!res[key];
